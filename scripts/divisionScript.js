257b75beb0999fe444c26dc33fedaf2a44e3105a
Julianna Messineo <juliannamessineo@gmail.com>
2026-01-24 21:55:43 -0600

    delete console logs

--- scripts/divisionScript.js
+++ scripts/divisionScript.js
@@ -76,7 +76,6 @@ openCloseNavButton.addEventListener('click', () => {
   if (sideNav.dataset.state === 'open') {
     sideNav.dataset.state = 'closed';
   } else sideNav.dataset.state = 'open';
-  console.log(sideNav.dataset.state);
 });
 
 divisionModelCheckbox.addEventListener('change', function () {
